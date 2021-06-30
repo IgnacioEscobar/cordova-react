15c127558290f0259fa0b622cf201f3c66f0c0c8
Ignacio Escobar <inec_14@hotmail.com>
2021-06-30 07:04:28 -0300

    Tester

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import Text from './Text';
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
+
+function BatteryStatus() {
+  const [status, setStatus] = useState(null)
+  useEffect(() => {
+    window.addEventListener("batterystatus", onBatteryStatus, false);
+    function onBatteryStatus(status) {
+      setStatus(status)
+    }
+  }, [])
+
+  return (
+    status != null ? <span>Bateria {status.level}% / {status.isPlugged ? "Conectado": "No conectado"}</span> : <span>Battery not available</span>
+  )
+}
 
 function App() {
   return (
@@ -10,6 +26,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Text/>
+        <BatteryStatus/>
         <a
           className="App-link"
           href="https://reactjs.org"
